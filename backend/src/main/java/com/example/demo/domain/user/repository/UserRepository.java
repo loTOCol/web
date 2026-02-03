f731ab8302908d5fd1ae4ec1bb9a49413daccee6
lotocol <cabbz987@naver.com>
2026-02-03 18:34:51 +0900

    User Repository 추가

---
+++ backend/src/main/java/com/example/demo/domain/user/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.domain.user.repository;
+
+import com.example.demo.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsById(UUID id);
+    boolean existsByEmail(String email);
+    boolean existsByName(String name);
+}
