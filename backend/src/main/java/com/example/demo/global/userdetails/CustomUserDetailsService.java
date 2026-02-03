d4c710ecb2f35c0151e1a5c455589d86d0a78cdb
lotocol <cabbz987@naver.com>
2026-02-03 18:37:03 +0900

    로그인 인증을 위한 CustomUserDetails 및 UserDetailsService 추가

--- backend/src/main/java/com/example/demo/global/userdetails/CustomUserDetailsService.java
+++ backend/src/main/java/com/example/demo/global/userdetails/CustomUserDetailsService.java
@@ -1,15 +1,49 @@
 package com.example.demo.global.userdetails;
 
+import com.example.demo.domain.user.entity.User;
+import com.example.demo.domain.user.repository.UserRepository;
+import com.example.demo.domain.user.role.UserStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
+        // DB에서 이메일을 기반으로 사용자 정보를 조회
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("해당 이메일을 찾을 수 없습니다." + email));
 
-        return null;
+        if(user.getStatus() == UserStatus.BANNED){
+            throw new LockedException("벤된 계정입니다.");
+        }
+
+        if(user.getStatus() == UserStatus.WITHDRAWAL){
+            throw new LockedException("탈퇴한 계정입니다.");
+        }
+
+        return new CustomUserDetails(user);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
