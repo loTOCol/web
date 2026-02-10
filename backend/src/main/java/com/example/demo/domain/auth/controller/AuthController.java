67929efad8667683424426f2fdb1e7bce0387683
lotocol <cabbz987@naver.com>
2026-02-10 18:53:36 +0900

    signUp 기능 추가

--- backend/src/main/java/com/example/demo/domain/auth/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/domain/auth/controller/AuthController.java
@@ -1,6 +1,14 @@
 package com.example.demo.domain.auth.controller;
 
+import com.example.demo.domain.auth.dto.request.SignUpRequest;
+import com.example.demo.domain.auth.service.AuthService;
+import com.example.demo.global.jwt.service.TokenService;
+import com.example.demo.global.response.RsData;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+    private final TokenService tokenService;
+
+    @PostMapping(value = "/signup", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public RsData<?> signUp(@RequestBody @Valid SignUpRequest request) {
+        authService.signUp(request);
+        return RsData.of("200", "회원가입 성공");
+    }
 }
