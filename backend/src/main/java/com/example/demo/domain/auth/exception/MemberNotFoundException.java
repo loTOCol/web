21978000b2f3acc40619b70da255a633aa876904
lotocol <cabbz987@naver.com>
2026-02-13 19:41:34 +0900

    MemberNotFoundException 중복 제거

--- backend/src/main/java/com/example/demo/domain/auth/exception/MemberNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.domain.auth.exception;
-
-import com.example.demo.global.exception.AuthException;
-
-public class MemberNotFoundException extends AuthException {
-    public MemberNotFoundException() {
-        super("회원이 존재하지 않습니다.","404");
-    }
-}
